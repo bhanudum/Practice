d61897ccc611950c908f78d0f8165bc12c88896c
bhanuchanderdumpala <banu.tsw@gmail.com>
2025-09-12 18:35:44 +0530

    New user

--- routes/newUserSignup.js
+++ routes/newUserSignup.js
@@ -6,30 +6,49 @@ console.log("frm signup")
     var randomNo = Math.floor(Math.random() * 10)
     console.log(randomNo)
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     var resObj = {};
     var userAccountDetails = req.body;
-    try {   
+    try {
+        // Check for existing email or mobile number
+        const duplicateUser = await dbUtils.doDbCommunication({
+            $or: [
+                { email: userAccountDetails.email },
+                { mobile: userAccountDetails.mobile }
+            ]
+        }, 'find', 'userAccountDetails');
 
-        
+        if (duplicateUser && duplicateUser.length > 0) {
+            if (duplicateUser.some(u => u.email === userAccountDetails.email)) {
+                resObj.msg = 'already email id is existing';
+            } else {
+                resObj.msg = 'mobile number already exists';
+            }
+            return res.send(JSON.stringify(resObj));
+        }
+
         bcrypt.hash(userAccountDetails.password, 5, function(err, hash) {
-            // Store hash in your password DB.
+            if (err) {
+                resObj.msg = 'Error';
+                return res.send(JSON.stringify(resObj));
+            }
             userAccountDetails.password = hash;
             dbUtils.doDbCommunication(userAccountDetails, 'insertOne', 'userAccountDetails').then((result) => {
-                console.log(result);
                 if(!result) {
                     resObj.msg = 'Error';
-
                 } else {
                     resObj.msg = 'Done';
                 }
                 res.send(JSON.stringify(resObj));
             }).catch((err) => {
-                
+                resObj.msg = 'Error';
+                res.send(JSON.stringify(resObj));
             });
         });
     } catch(err) {
-        console.log(err)
+        console.log(err);
+        resObj.msg = 'Error';
+        res.send(JSON.stringify(resObj));
     }
 });
 
